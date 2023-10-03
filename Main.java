44908990fbd564df7b38696867df1e39b42fbd33
Edwin <edwinchuajinrui@gmail.com>
2023-10-03 14:16:07 +0800

    fuk

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main
         System.out.println("Hello world");
         System.out.println("Fuck you");
         System.out.println("This is java assignment");
+        System.out.println("I am yukang");
         System.out.println("I am edwin");
+        System.out.println();
     }
 }
