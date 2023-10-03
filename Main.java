0f2d9673e124918c97b78bf0164056eeae57ad9b
Edwin <edwinchuajinrui@gmail.com>
2023-10-03 14:10:55 +0800

    chenge

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main
         System.out.println("Hello world");
         System.out.println("Fuck you");
         System.out.println("This is java assignment");
-        System.out.println("I am yukang");
         System.out.println("I am edwin");
     }
 }
