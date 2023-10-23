157076770e687fb68cef3c89a86080588a527682
Edwin <edwinchuajinrui@gmail.com>
2023-10-23 13:34:58 +0800

    added changes to book

--- Book.java
+++ Book.java
@@ -1,4 +1,31 @@
 public class Book 
 {
-    private String isbn;
+    private String bookTitle, bookIsbn, author, publisher,  bookBorrowedDate, bookReturnDate;
+    private double bookPrice;
+    private boolean bookAvailability;
+
+    //default constructor
+    public Book() 
+    {
+        this(null, null, null, null, null, null, 0.00, true);
+    }
+
+    //parameterized constructor
+    public Book(String bookTitle, String bookIsbn, String author, String publisher, String bookBorrowedDate, String bookReturnDate, double bookPrice, boolean bookAvailability)
+    {
+        setBookDetail(bookTitle, bookIsbn, author, publisher, bookBorrowedDate, bookReturnDate, bookPrice, bookAvailability);
+    }
+
+    public void setBookDetail(String bookTitle, String bookIsbn, String author, String publisher, String bookBorrowedDate, String bookReturnDate, double bookPrice, boolean bookAvailability)
+    {
+        this.bookTitle = bookTitle;
+        this.bookIsbn = bookIsbn;
+        this.author = author;
+        this.publisher = publisher;
+        this.bookPrice = bookPrice;
+        this.bookAvailability = bookAvailability;
+        this.bookBorrowedDate = bookBorrowedDate;
+        this.bookReturnDate = bookReturnDate;
+    }
+
 }
