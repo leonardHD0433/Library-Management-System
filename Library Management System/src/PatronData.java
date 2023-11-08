f376f1b4c165517f8ede94a09e3314df209937cf
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2023-11-08 09:29:30 +0800

    yes

--- Library Management System/src/PatronData.java
+++ Library Management System/src/PatronData.java
@@ -4,5 +4,5 @@ public interface PatronData
     public final String[] PATRON_ID = {"0136546","0136323", "0134832", "0136441", "0136484"};
     public final String[] PATRON_CONTACT_NO = {"012-7777651", "014-9892509", "012-2012573", "016-6889918", "017-8009904"};
 
-    public void setPatronDetail(int i);
+    public void setPatronList(int i);
 }
