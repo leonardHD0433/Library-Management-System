bb953fcc21af0f45e7836ef313bf60972d30c8f8
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2023-11-20 20:32:11 +0800

    changes

--- Library Management System/src/LibraryManagementSystem.java
+++ Library Management System/src/LibraryManagementSystem.java
@@ -1,5 +1,5 @@
 import java.io.*;
-public class Main  
+public class LibraryManagementSystem  
 {   
     static Session session;
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException
